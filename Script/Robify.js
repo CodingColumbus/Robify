6e289800f1047e3b769f8a5c4d6abf6874cb7a91
CodingColumbus <robinweiss2010@gmail.com>
2022-08-18 11:02:04 +0200

    init

---
+++ Script/Robify.js
@@ -0,0 +1,6 @@
+
+
+
+// html5 Dateiupload -> Dateien aber nicht hochladen, sondern von den ausgewählten Dateien die Infos lesen (Name, Dateityp, Pfad)
+// gelesene Dateiinfos in json Datei speichern
+// json Datei auslesen und html5 audio Tags erzeugen
